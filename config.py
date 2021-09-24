682e852abc514c49efd1144cf1daaa7a2bdd406b
jean PORCHER <jean.porcher@socgen.com>
2021-09-24 17:54:40 +0200

    fix ...

--- config.py
+++ config.py
@@ -5,7 +5,3 @@ DEBUG = True
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
 
-# Define the database - we are working with
-# SQLite for this example
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-DATABASE_CONNECT_OPTIONS = {}
